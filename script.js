4af09d8d51f1da2db70ea36f4f053539941bf057
rez <rez@frontierml.com>
2020-01-30 10:30:56 -0600

    Finish

--- script.js
+++ script.js
@@ -5,16 +5,22 @@ let story = document.querySelector('.story');
 let video__mute = document.querySelector('.video__mute');
 let video__button = document.querySelector('.video__button');
 
+let features = document.querySelectorAll('.feature');
+
 let played = false;
 
 window.addEventListener('scroll', function() {
-	console.log(window.innerHeight * .85);
-	console.log(story.offsetTop);
 	if((window.innerHeight * .85) <= window.pageYOffset && played == false) {
 		played = true;
 		video.play();
-
 	}
+	features.forEach(function(feature, index) {
+		let bottomScroll = window.pageYOffset + window.innerHeight;
+		console.log((feature.offsetHeight / 2));
+		if(feature.offsetTop + (feature.offsetHeight / 1.75) <= bottomScroll) {
+			feature.classList.add('fade');
+		}
+	})
 })
 
 video.addEventListener('click', function() {
